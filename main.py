79034bd9b852d258011416255efa11d3f618d7ec
LOGIKA <daniiltrotsenko2002@gmail.com>
2023-10-28 14:22:54 +0300

    created window maket

--- main.py
+++ main.py
@@ -3,5 +3,19 @@ from PIL import Image
 from PIL import ImageFilter
 
 
+class ImageEditor:
+    def __init__(self, filename):
+        self.filename = filename
+        self.original = None
+        self.edited = []
+
+    def open(self):
+        try:
+            self.original = Image.open(self.filename)
+        except:
+            QMessageBox.warning(window, "Помилка", "Файл не знайдено!")
+        self.original.show()
+
+
 window.show()
 app.exec_()
